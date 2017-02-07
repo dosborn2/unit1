184b4e9f325e8c7ca2a104dd8fee66b4770d2f5a
dosborn2 <dosborn2@wisc.edu>
2017-02-06 18:13:48 -0600

    Module 3 activity completed
    ajax and callback function debug

--- js/debug_ajax.js
+++ js/debug_ajax.js
@@ -1,20 +1,20 @@
 function debugCallback(response){
-	
+
 	$(mydiv).append('GeoJSON data: ' + JSON.stringify(mydata));
 };
 
 function debugAjax(){
-	
+
 	var mydata;
 
 	$.ajax("data/MegaCities.geojson", {
 		dataType: "json",
 		success: function(response){
-			
+
 			debugCallback(mydata);
 		}
 	});
-
+// br signifies line break
 	$(mydiv).append('<br>GeoJSON data:<br>' + JSON.stringify(mydata));
 };
 
